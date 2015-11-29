158f1f10099453eced05596194c9d6a80036de86
Jamerson Lima <jamersonfpl@gmail.com>
2015-11-29 11:22:29 -0300

    Script e arquivo CSV de dados extraídos de pregões entre 2013 e 2015.

---
+++ scripts/extrair_licitacoes_cafe.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import xml.etree.ElementTree as etree
+import urllib2
+
+r = urllib2.urlopen("http://compras.dados.gov.br/licitacoes/v1/licitacoes.xml?data_publicacao_min=2013-01-01&item_material=9725")
+content = r.read()
+
+output_licitacoes = open("licitacoes_cafe_2013_2015.csv", "w")
+output_pregoes = open("pregoes_cafe_2013_2015.csv", "w")
+output_licitacoes.write("id;data_abertura_proposta;data_entrega_edital;data_entrega_proposta;data_publicacao;endereco_entrega_edital;funcao_responsavel;identificador;informacoes_gerais;modalidade;nome_responsavel;numero_aviso;numero_itens;numero_processo;objeto;situacao_aviso;tipo_pregao;tipo_recurso;uasg;beneficio;cnpj_fornecedor;codigo_item_material;codigo_item_servico;cpfVencedor;criterio_julgamento;decreto_7174;descricao_item;modalidade;numero_aviso;numero_item_licitacao;numero_licitacao;quantidade;sustentavel;uasg;unidade_valor_estimado;url_licitacao\n")
+output_pregoes.write("id;data_abertura_proposta;data_entrega_edital;data_entrega_proposta;data_publicacao;endereco_entrega_edital;funcao_responsavel;identificador;informacoes_gerais;modalidade;nome_responsavel;numero_aviso;numero_itens;numero_processo;objeto;situacao_aviso;tipo_pregao;tipo_recurso;uasg;descricao_item;quantidade_item;valor_estimado_item;descricao_detalhada_item;margem_preferencial;situacao_item;menor_lance;decreto_7174;fornecedor_vencedor;tratamento_diferenciado;unidade_fornecimento;valor_negociado\n")
+
+tree = etree.fromstring(content)
+root = tree[1]
+
+resources = tree[1]
+
+countpregao = 1
+countlicitacao = 1
+count_lic = 1
+
+for lic in range(len(resources)):
+    licitacao = resources[lic]
+
+    links = licitacao.find("_links")
+    items_list = ["beneficio", "cnpj_fornecedor", "codigo_item_material", "codigo_item_servico", "cpfVencedor", "criterio_julgamento", "decreto_7174", "descricao_item", "modalidade", "numero_aviso", "numero_item_licitacao", "numero_licitacao", "quantidade", "sustentavel", "uasg", "unidade", "valor_estimado"]
+    url = ""
+    id_pregao = ""
+
+    if licitacao.find('modalidade').text == '5':
+        id_pregao = licitacao.find('identificador').text[:7] + licitacao.find('identificador').text[8:]
+        url = 'http://compras.dados.gov.br/pregoes/doc/pregao/' + id_pregao + '/itens.xml'
+        items_list = ["descricao_item", "quantidade_item", "valor_estimado_item", "descricao_detalhada_item", "margem_preferencial", "situacao_item", "menor_lance", "decreto_7174", "fornecedor_vencedor", "tratamento_diferenciado", "unidade_fornecimento", "valor_negociado"]
+    else:
+        for link in links:
+            if link.attrib['rel'] == 'self':
+                url = 'http://compras.dados.gov.br' + link.attrib['href'] + '/itens.xml'
+                break
+    s = urllib2.urlopen(url)
+    content = s.read()
+    item_root = etree.fromstring(content)
+    item_resources = item_root[1]
+    for item in item_resources:
+        st = ""
+        if licitacao.find('modalidade').text == '5':
+            st = "\"%s\";" % countpregao
+        else:
+            st = "\"%s\";" % countlicitacao
+
+        licitacoes_list = ["data_abertura_proposta", "data_entrega_edital", "data_entrega_proposta", "data_publicacao", "endereco_entrega_edital", "funcao_responsavel", "identificador", "informacoes_gerais", "modalidade", "nome_responsavel", "numero_aviso", "numero_itens", "numero_processo", "objeto", "situacao_aviso", "tipo_pregao", "tipo_recurso", "uasg"]
+        for licitacao_name in licitacoes_list:
+            if licitacao.find(licitacao_name) is not None:
+                if licitacao.find(licitacao_name).text is not None:
+                    try:
+                        st += ("\"" + licitacao.find(licitacao_name).text.replace('\"', " polegadas") + "\"" + ';')
+                    except:
+                        print "oie foi erro"
+                        import ipdb; ipdb.set_trace()
+
+                else:
+                    st += "\"NA\";"
+            else:
+                st += "\"NA\";"
+        for item_name in items_list:
+            if item.find(item_name) is not None:
+                if item.find(item_name).text is not None:
+                    try:
+                        st += ("\"" + item.find(item_name).text.replace('\"', " polegadas") + "\"" + ';')
+                    except:
+                        print "oie foi erro"
+                        import ipdb; ipdb.set_trace()
+                else:
+                    st += "\"NA\";"
+            else:
+                st += "\"NA\";"
+
+        url_licitacao = ""
+        if licitacao.find('modalidade').text == '5':
+            id_pregao = licitacao.find('identificador').text[:7] + licitacao.find('identificador').text[8:]
+            url = 'http://compras.dados.gov.br/pregoes/doc/pregao/' + id_pregao + '/itens.xml'
+            items_list = ["descricao_item", "quantidade_item", "valor_estimado_item", "descricao_detalhada_item", "margem_preferencial", "situacao_item", "menor_lance", "decreto_7174", "fornecedor_vencedor", "tratamento_diferenciado", "unidade_fornecimento", "valor_negociado"]
+            url_licitacao = '/pregoes/doc/pregao/' + id_pregao
+        else:
+            for link in links:
+                if link.attrib['rel'] == 'self':
+                    url = 'http://compras.dados.gov.br' + link.attrib['href'] + '/itens.xml'
+                    break
+            url_licitacao = link.attrib['href']
+        st += "\"" + url_licitacao + "\""
+        st = st.replace('\n', '').replace('\t', '').replace('\r', '').replace('Ž'.decode('utf-8'), '')
+
+        if licitacao.find('modalidade').text == '5':
+            countpregao += 1
+            output_pregoes.write(st.encode("utf-8") + '\n')
+            print "id=" + str(countpregao) + " pregao " + str(count_lic) + " & item " + id_pregao
+        else:
+            countlicitacao += 1
+            output_licitacoes.write(st.encode("utf-8") + '\n')
+            print "id=" + str(countlicitacao) + " licitacao " + str(count_lic) + " & item " + licitacao.find("identificador").text
+    count_lic += 1
+output_licitacoes.close()
+output_pregoes.close()
