1f7d8c0a77722cfeb9856db08275e12c4d4b3fa1
Luiz Braga <luizbragaa@gmail.com>
2015-12-03 10:29:20 -0300

    url dos estaticos

--- harpia/harpia/urls.py
+++ harpia/harpia/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from harpia import settings
+from django.conf.urls import patterns
+
 
 urlpatterns = [
     # Examples:
@@ -8,3 +11,9 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += patterns(
+    '',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT}),
+    )
