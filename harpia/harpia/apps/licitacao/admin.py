3a0370edf2132573228f318c778edd4d5a49cf99
Luiz Braga <luizbragaa@gmail.com>
2015-12-03 14:51:39 -0300

    Modelos

--- harpia/harpia/apps/licitacao/admin.py
+++ harpia/harpia/apps/licitacao/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
 
-# Register your models here.
+from django.contrib import admin
+from licitacao.models import ItensLicitacao
+from licitacao.models import Licitacao
+
+
+class LicitacaoAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Licitacao, LicitacaoAdmin)
+
+
+class ItensLicitacaoAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(ItensLicitacao, ItensLicitacaoAdmin)
--- harpia/harpia/apps/licitacao/models.py
+++ harpia/harpia/apps/licitacao/models.py
@@ -4,36 +4,63 @@ from django.db import models
 
 
 class Licitacao(models.Model):
-    data_abertura_proposta = models.DateField(
+    dataAberturaProposta = models.DateTimeField(
         verbose_name=u'data de abertura da proposta', null=True, blank=True)
-    data_entrega_edital = models.DateField(
+    dataEntregaEdital = models.DateTimeField(
         verbose_name=u'data de entrega do edital', null=True, blank=True)
-    data_entrega_proposta = models.DateField(
+    dataEntregaProposta = models.DateTimeField(
         verbose_name=u'data de entrega da proposta', null=True, blank=True)
-    data_publicacao = models.DateField(
+    dataPublicacao = models.DateField(
         verbose_name=u'data de publicacao', null=True, blank=True)
-    endereco_entrega_edital = models.TextField(
+    enderecoEntregaEdital = models.TextField(
         max_length=255, blank=True, null=True)
-    funcao_responsavel = models.TextField(
+    funcaoResponsavel = models.TextField(
         max_length=255, blank=True, null=True)
     identificador = models.TextField(
         max_length=255, blank=True, null=True)
-    informacoes_gerais = models.TextField(
+    informacoesGerais = models.TextField(
         max_length=255, blank=True, null=True)
     modalidade = models.TextField(
         max_length=255, blank=True, null=True)
-    nome_responsavel = models.TextField(
+    nomeResponsavel = models.TextField(
         max_length=255, blank=True, null=True)
-    numero_aviso = models.PositiveIntegerField(u'numero de aviso')
-    numero_itens = models.PositiveIntegerField(u'numero de itens')
-    numero_processo = models.PositiveIntegerField(u'numero do processo')
+    numeroAviso = models.PositiveIntegerField(u'numero de aviso')
+    numeroItens = models.PositiveIntegerField(u'numero de itens')
+    numeroProcesso = models.TextField(
+        max_length=255, blank=True, null=True)
     objeto = models.TextField(
         max_length=255, blank=True, null=True)
-    situacao_aviso = models.TextField(
+    situacaoAviso = models.TextField(
         max_length=255, blank=True, null=True)
-    tipo_pregao = models.TextField(
+    tipoPregao = models.TextField(
         max_length=255, blank=True, null=True)
-    tipo_recurso = models.TextField(
+    tipoRecurso = models.TextField(
         max_length=255, blank=True, null=True)
     uasg = models.TextField(
         max_length=255, blank=True, null=True)
+
+
+class ItensLicitacao(models.Model):
+    descricao_item = models.TextField(max_length=255, null=True, blank=True)
+    valor_estimado_item = models.TextField(
+        max_length=255, null=True, blank=True)
+    descricao_detalhada_item = models.TextField(
+        max_length=255, null=True, blank=True)
+    margem_preferencial = models.TextField(
+        max_length=255, null=True, blank=True)
+    situacao_item = models.TextField(max_length=255, null=True, blank=True)
+    menor_lance = models.TextField(max_length=255, null=True, blank=True)
+    decreto_7174 = models.TextField(max_length=255, null=True, blank=True)
+    fornecedor_vencedor = models.TextField(
+        max_length=255, null=True, blank=True)
+    tratamento_diferenciado = models.TextField(
+        max_length=255, null=True, blank=True)
+    unidade_fornecimento = models.TextField(
+        max_length=255, null=True, blank=True)
+    url_pregao = models.TextField(max_length=255, null=True, blank=True)
+    tipo_material = models.TextField(max_length=255, null=True, blank=True)
+    quantidade_item = models.TextField(max_length=255, null=True, blank=True)
+    situacao = models.TextField(max_length=255, null=True, blank=True)
+    valor_negociado = models.FloatField(null=True, blank=True)
+    valor_final = models.FloatField(null=True, blank=True)
+    valor_item = models.FloatField(null=True, blank=True)
